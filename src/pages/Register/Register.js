6dc020eb722daea60b258d309b70cadeb499540c
SabikunNaharTanha <sabikunnahartanha6@gmail.com>
2022-08-01 20:52:51 +0600

    created register form

--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -1,12 +1,49 @@
 import React from 'react';
+import { Form } from 'react-bootstrap';
+import Button from 'react-bootstrap/Button';
+import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+
 
 const Register = () => {
-    return (
-        <div>
-            <h2>Please Register</h2>
+    const navigate = useNavigate();
+    const navigateLogin = () => {
+        navigate('/login');
+    }
 
+    const handleRegister = event => {
+        event.preventRegister();
+    }
+    return (
+        <div className='container w-50 mx-auto'>
+            <h2 className='text-center m-4 text-primary'>Please Login</h2>
+            <Form onSubmit={handleRegister}>
+                <Form.Group className="mb-3" >
+                    <Form.Control type="email" name='name' placeholder="Enter Your Name" required />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Control type="email" placeholder="Enter email" required />
+                    <Form.Text className="text-muted">
+                        We'll never share your email with anyone else.
+                    </Form.Text>
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+
+                    <Form.Control type="password" placeholder="Password" required />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                    <Form.Check type="checkbox" label="Check me out" />
+                </Form.Group>
+                <Button variant="primary" type="submit" >
+                    Register
+                </Button>
+
+            </Form>
+            <p className='mt-3'>Already Registered? <Link className='text-primary text-decoration-none' onClick={navigateLogin}> Please Login</Link> </p>
         </div>
     );
 };
 
-export default Register;
+export default Register; 
